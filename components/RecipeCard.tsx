ec806870d01375baa65bbc1ba7af34486ccb7da9
maliha63 <malihabathoolc@gmail.com>
2025-11-20 16:30:22 +0530

    Update RecipeCard.tsx

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import type { Recipe } from '../types';
 
@@ -35,10 +36,11 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe, onSelectRecipe }) => {
             <span>{recipe.prepTime}</span>
           </div>
           <div className="flex items-center space-x-1">
-             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-cyan-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+             {/* Fire Icon for Calories */}
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-cyan-400" viewBox="0 0 24 24" fill="currentColor">
+                <path fillRule="evenodd" d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
             </svg>
-             <span>{recipe.calories} kcal</span>
+             <span>{recipe.calories.replace(/ ?kcal/i, '')} kcal</span>
           </div>
         </div>
       </div>
